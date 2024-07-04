9feda9a35164b0df5b73210569318926875c8ce6
Amusah <henryamusah@gmail.com>
2024-07-04 22:13:56 +0000

    responsive nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { navLinks } from '../constants';
 import { shared } from '../assets';
 import styles from './Navbar.module.scss';
 
-const { logoDark, logoLight } = shared.desktop;
+const [ { logoDark, logoLight }, { close, hamburger } ] = shared;
+
+
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 660);
+
+  
+  function checkMobileView(){
+    setIsMobile(window.innerWidth <= 660);
+  }
+
+  function handleToggleNav(){
+    setToggle(toggle => !toggle);
+  }
+
+  useEffect(() => {
+   // checkMobileView();
+
+    window.addEventListener('resize', checkMobileView);
+    return () => window.removeEventListener("resize", checkMobileView);
+  }, []);
+
   return (
     <nav className={`${styles.nav}`}>
       <img className={styles.navLogo} src={logoDark} alt="logo" />
-      <ul className={styles.navLinks}>
+      <ul className={`${styles.navLinks} ${toggle ? 'show' : ''}`}>
         {navLinks.map((link, idx) => (
-          <li key={link.id} className={`${idx === navLinks.length - 1 ? '' : styles.mr}`} >{link.title}</li>
+          <li key={link.id} className={`${styles.link} ${idx === navLinks.length - 1 ? '' : styles.mr}`} onClick={isMobile ? handleToggleNav : undefined} >{link.title}</li>
         ))}
       </ul>
+     {isMobile && <img onClick={handleToggleNav} src={toggle ? close : hamburger} alt="hamburger icon" />}
     </nav>
   )
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,25 @@
-import friendly from './home/desktop/friendly.svg';
-import passionate from './home/desktop/passionate.svg';
-import resourceful from './home/desktop/resourceful.svg';
-import logoDark from './shared/desktop/logo-dark.png';
-import logoLight from './shared/desktop/logo-light.png';
+import friendly from "./home/desktop/friendly.svg";
+import passionate from "./home/desktop/passionate.svg";
+import resourceful from "./home/desktop/resourceful.svg";
+import logoDark from "./shared/desktop/logo-dark.png";
+import logoLight from "./shared/desktop/logo-light.png";
+import close from "./shared/mobile/icon-close.svg";
+import hamburger from "./shared/mobile/icon-hamburger.svg";
 
 export const home = {
   desktop: { friendly, passionate, resourceful },
-}
+};
 
-export const shared = {
-  desktop: { logoDark, logoLight },
-}
+// export const shared = {
+//   desktop: { logoDark, logoLight },
+
+//   mobile: { close, hamburger }
+// }
+
+export const shared = [
+  { logoDark, logoLight },
+
+  { close, hamburger },
+];
 
 // export { friendly, passionate, resourceful };
